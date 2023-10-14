8c5cbb7f7c126955cce6de8904989019eb50c1f0
nksm-npe <notpoorelephant@gmail.com>
2023-10-14 16:39:32 +0900

    heic あいお

--- src/views/PhotoAndMap.vue
+++ src/views/PhotoAndMap.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import EXIF from 'exif-js'
-import loadImage from "blueimp-load-image";
 
-
+import ExifReader from 'exifreader'
 const imgFile = ref<File | null>(null)
 const imgFileUrl = ref<string>('')
-const imgTags = ref<any>(null)
-const dropHandler = (ev: DragEvent) => {
+const laDecimal2 = ref<number>(0)
+const loDecimal2 = ref<number>(0)
+const dropHandler = async (ev: DragEvent) => {
   console.log('File(s) dropped')
 
   // 既定の動作で防ぐ（ファイルが開かれないようにする）
@@ -15,58 +14,24 @@ const dropHandler = (ev: DragEvent) => {
 
   if (ev.dataTransfer?.items) {
     // DataTransferItemList インターフェイスを使用して、ファイルにアクセスする
-    ;[...ev.dataTransfer.items].forEach((item, i) => {
+    ;[...ev.dataTransfer.items].forEach(async (item, i) => {
       // ドロップしたものがファイルでない場合は拒否する
       if (item.kind === 'file') {
         const file = item.getAsFile()
-        console.log(`… file[${i}].name = ${file?.name}`)
+        if (!file) return
+        const tags = await ExifReader.load(file)
+        console.log('tags', tags)
+        console.log(tags.GPSLatitude?.description)
+        laDecimal2.value = parseFloat(tags.GPSLatitude?.description as string)
+        loDecimal2.value = parseFloat(tags.GPSLongitude?.description as string)
+
         imgFile.value = file
         imgFileUrl.value = URL.createObjectURL(imgFile.value)
-        console.log(imgFile)
-        // EXIF.getData(imgFileUrl.value, function () {
-        //   imgTags.value = EXIF.getAllTags(this)
-        //   console.log('imgTags:' + imgTags.value)
-        // })
-        loadImage.parseMetaData(imgFile.value, function (data) {
-
-          if (data.exif) {
-            // options.orientation = data.exif.get('Orientation');
-            console.log("exif:"+data.exif);
-            // console.log("GPSLongitude"+ data.exif.get("GPSLatitude"))
-            // console.log("GPSLatitude"+ data.exif.get("GPSLatitude"))
-            console.log("gpsinfo:",data.exif.get('GPSInfo'))
-            const gpsInfo = data.exif.get('GPSInfo')
-            console.log("getAll",gpsInfo.getAll());
-            console.log("GPSLatitude",gpsInfo.get('GPSLatitude'))
-            const [laDegrees,laMinutes,laSeconds] = gpsInfo.get('GPSLatitude')
-            console.log(typeof laDegrees,laMinutes,laSeconds)
-            const laDecimal = laDegrees + laMinutes / 60 + laSeconds / 3600
-            console.log(laDecimal)
-            console.log("GPSLongitude",gpsInfo.get('GPSLongitude'))
-            const [loDegrees,loMinutes,loSeconds] = gpsInfo.get('GPSLongitude')
-            const loDecimal = loDegrees + loMinutes / 60 + loSeconds / 3600          
-            console.log(loDecimal)
-          }
-          // this.displayImage(file, options);
-          console.log("data", data)
-          console.log("Exif data: ", data.exif);
-          
-        });
-        
       }
     })
-  } else {
-    // DataTransfer インターフェイスを使用してファイルにアクセスする
-    // ;[...ev.dataTransfer?.files].forEach((file, i) => {
-    //   console.log(`… file[${i}].name = ${file.name}`)
-    // })
   }
 }
-function getExifData() {
-  return new Promise((resolve) => {
-    EXIF.getAllTags
-  })
-}
+
 const dragOverHandler = (ev: DragEvent) => {
   console.log('File(s) in drop zone')
 
@@ -85,7 +50,7 @@ const dragOverHandler = (ev: DragEvent) => {
   </div>
 
   <img v-if="imgFile != null" :src="imgFileUrl" :alt="imgFile.name" :title="imgFile.name" />
-  {{ imgTags }}
+  {{ laDecimal2 }}, {{ loDecimal2 }}
 </template>
 <style scoped>
 #drop_zone {
@@ -93,15 +58,15 @@ const dragOverHandler = (ev: DragEvent) => {
   width: 200px;
   height: 100px;
 }
-img{
-    width: 180px;
-    height: 60px;
+img {
+  width: 180px;
+  height: 60px;
 
-    /* // 左端に寄せてサイズを見やすくしたかったので、設定 */
-    object-position: left;
+  /* // 左端に寄せてサイズを見やすくしたかったので、設定 */
+  object-position: left;
 
-    /* // imgのサイズを分かりやすくするため、background-colorを設定 */
-    background-color: #999;
-    object-fit: contain;
+  /* // imgのサイズを分かりやすくするため、background-colorを設定 */
+  background-color: #999;
+  object-fit: contain;
 }
 </style>
